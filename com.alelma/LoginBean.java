86e395903aaaa001a8d591835775612acc1aee77
Mehmet Gülsoy <m.mehmetgulsoy@gmail.com>
2018-02-18 09:15:57 +0300

    Deneme

---
+++ com.alelma/LoginBean.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alelma;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            request.login(this.username, this.password);
+
+            return "/private/UrunKayit.xhtml?faces-redirect=true";
+        } catch (ServletException e) {
+             
+            //request.isUserInRole(username)
+            if (!e.getMessage().equals("This request has already been authenticated")) {
+
+                context = FacesContext.getCurrentInstance();
+                context.addMessage(null, new FacesMessage("E-Posta ve/veya şifre hatalı."));
+
+            }
+        }
+        return "";
+    }
+
+    public void logout() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Logout failed."));
+        }
+    }
+}
---
+++ com.alelma/urunMail.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alelma;
+
+import java.util.*;
+import java.io.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author Mehmet
+ */
+@ManagedBean
+@SessionScoped
+public class urunMail {
+
+    static String msgText1 = "This is a message body.\nHere's line two.";
+    static String msgText2 = "This is the text in the message attachment.";
+
+    public void mail() {
+
+        String to = "m.mehmetgulsoy@gmail.com";
+        String from = "mehmet@liman.com.tr";
+        String host = "mail.liman.com.tr";
+        boolean debug = true;
+        // create some properties and get the default Session
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.auth", "true");
+        //props.put("mail.smtp.user", "mehmet@liman.com.tr");
+        //props.put("mail.smtp.password", "3141");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("mehmet@liman.com.tr", "3141");
+            }
+        });
+
+        session.setDebug(debug);
+        try {
+            // create a message
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject("JavaMail APIs Multipart Test");
+            msg.setSentDate(new Date());
+            // create and fill the first message part
+            MimeBodyPart mbp1 = new MimeBodyPart();
+            mbp1.setText(msgText1);
+            // create and fill the second message part
+            MimeBodyPart mbp2 = new MimeBodyPart();
+            // Use setText(text, charset), to show it off !
+            mbp2.setText(msgText2, "us-ascii");
+            // create the Multipart and its parts to it
+            Multipart mp = new MimeMultipart();
+            mp.addBodyPart(mbp1);
+            mp.addBodyPart(mbp2);
+            // add the Multipart to the message
+            msg.setContent(mp);
+            // send the message
+            Transport.send(msg);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+            Exception ex = null;
+            if ((ex = mex.getNextException()) != null) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void mail2() throws NamingException, AddressException, MessagingException {
+        
+        
+        Context initCtx = new InitialContext();
+        Context envCtx = (Context) initCtx.lookup("java:comp/env");
+        Session session = (Session) envCtx.lookup("mail/Session");
+        
+        String from = "mehmet@liman.com.tr";
+         
+        String to = "m.mehmetgulsoy@gmail.com";
+        
+        try {
+            // create a message
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject("JavaMail APIs Multipart Test");
+            msg.setSentDate(new Date());
+            // create and fill the first message part
+            MimeBodyPart mbp1 = new MimeBodyPart();
+            mbp1.setText(msgText1);
+            // create and fill the second message part
+            MimeBodyPart mbp2 = new MimeBodyPart();
+            // Use setText(text, charset), to show it off !
+            mbp2.setText(msgText2, "us-ascii");
+            // create the Multipart and its parts to it
+            Multipart mp = new MimeMultipart();
+            mp.addBodyPart(mbp1);
+            mp.addBodyPart(mbp2);
+            // add the Multipart to the message
+            msg.setContent(mp);
+            // send the message
+            Transport.send(msg);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+            Exception ex = null;
+            if ((ex = mex.getNextException()) != null) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+}
